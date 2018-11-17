ef65556d2a3d07532c2a88ddbaa88cbeaa270a33
Yoichi Kawasaki <yoichika@microsoft.com>
2018-11-17 15:06:08 +0900

    Added test code

---
+++ tests/test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+[[ -n $DEBUG ]] && set -x -e
+
+cwd=`dirname "$0"`
+
+echo "Import files into configmap \"mycm\""
+kubectl create configmap mycm --from-file ${cwd}/files
+
+echo "Export configmap \"mycm\" into files in directry \"exports\""
+if [ ! -d "${cwd}/exports" ]; then
+  mkdir -p ${cwd}/exports
+fi
+${cwd}/../kube-configmap-exporter mycm -t exports
+
+echo "Check exported files"
+for i in $(ls -1 ${cwd}/files/*)
+do
+  f=$(basename $i)
+  o="${cwd}/exports/${f}"
+  if [ ! -f ${o} ]; then
+    echo "Error: ${f} wasn't exported as expected" >&2
+    exit 1
+  fi
+  BEFORE=$(shasum -a 256 $i |awk '{print $1}')
+  AFTER=$(shasum -a 256 $o|awk '{print $1}')
+  if [ "${BEFORE}" != "${AFTER}" ];then
+    echo "Error: exported file isn't the same as the original one: ${f}" >&2
+    exit 1
+  fi
+done
+
+echo "Success!"
